4e4a57697f6d5f48fc218e48ae1be862936f205a
Lasha027 <lasha.urekadze@gmail.com>
2025-04-04 19:36:47 +0200

    letsPlay

---
+++ homeWork3.js
@@ -0,0 +1,10 @@
+function zeronum(m,n) {
+    if(m<n) {
+        return n;}
+        else if(m>n){
+            return m;
+        }
+        else {
+            return 0;} 
+}
+console.log(zeronum(7,5))
